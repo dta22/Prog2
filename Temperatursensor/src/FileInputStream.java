dad19086ba290d88ffae053a0917988f72d16c16
admin <admin@DESKTOP-DNLFJVU.local>
2020-04-14 22:18:53 +0200

    test

--- Temperatursensor/src/FileInputStream.java
+++ Temperatursensor/src/FileInputStream.java
@@ -1,7 +1,12 @@
 public class FileInputStream {
 
 	public static void main(String[] args) {
-
+		InputStream f = new FileInputStream(dateiname);^
+		
+		byte[] buffer = new byte[(int) f.length()];
+		InputStream in = new FileInputStream(f);
+		in.read(buffer);
+		in.close();
 	}
 
 }
